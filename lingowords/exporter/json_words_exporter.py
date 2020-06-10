1902ca5e44a94099de591664b8d18c4d9eb28aac
Sem Bogaarts <sembogaarts@gmail.com>
2020-06-10 12:09:23 +0200

    Added JSON Export support

--- lingowords/exporter/json_words_exporter.py
+++ lingowords/exporter/json_words_exporter.py
@@ -1,7 +1,30 @@
 from lingowords.exporter.words_exporter import WordsExporter
+import json
+import os
 
 
 class JsonWordsExporter(WordsExporter):
+    filename = "lingwords_export.json"
+    path = "~/Desktop"
+
+    def ask(self):
+        # Show options to the user
+        print("Path to store file (default: (~/Desktop))")
+        path = input("Path: ")
+        # If overwrite
+        if path:
+            self.path = os.path.join(path, self.filename)
+        else:
+            self.path = os.path.join(os.path.expanduser('~'), 'Desktop', self.filename)
 
     def store(self, words):
-        pass
+        # Ask the user where to store the file
+        self.ask()
+        # Create sample file
+        file = open(self.path, 'w')  # Trying to create a new file or open one
+        # Write all words
+        y = json.dumps(words)
+
+        file.write(y)
+
+        file.close()
