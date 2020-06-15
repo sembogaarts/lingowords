703bdee9024311aebbeab650ad845773dd90753e
Sem Bogaarts <sembogaarts@gmail.com>
2020-06-16 00:22:36 +0200

    Updated test

--- test/test_reader_detector.py
+++ test/test_reader_detector.py
@@ -2,7 +2,6 @@ import unittest
 from unittest.mock import patch
 from lingowords.detectors.reader_detector import ReaderDetector
 from lingowords.reader.text_words_reader import TextWordReader
-from lingowords.reader.json_words_reader import JsonWordsReader
 
 
 class TestImportDetector(unittest.TestCase):
@@ -14,11 +13,3 @@ class TestImportDetector(unittest.TestCase):
         # Chose for MySQL
         text_reader = reader_detector.ask()
         self.assertEqual(text_reader, TextWordReader)
-
-    @patch('builtins.input', return_value='2')
-    def test_ask_json(self, input):
-        # Creates a new reader detector
-        reader_detector = ReaderDetector()
-        # Chose for MySQL
-        json_reader = reader_detector.ask()
-        self.assertEqual(json_reader, JsonWordsReader)
